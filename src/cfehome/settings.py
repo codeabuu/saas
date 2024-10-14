5ae16ce6c8e500e878cbe9c4c22f07cda04ab4a0
codeabuu <abdulngui718@gmail.com>
2024-10-14 18:30:59 -0400

    updates

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,9 +9,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!$az0ephd9*eal^q7hd$!onmx$%m2)b89fd@73x-+1r%$i(#01'
+SECRET_KEY=config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = True
+
 DEBUG = True
 
 ALLOWED_HOSTS = [

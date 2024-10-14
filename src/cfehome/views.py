bf7c46b68245b4f8b072febdfecd2eb416bdef61
codeabuu <abdulngui718@gmail.com>
2024-10-14 16:59:55 -0400

    main project

---
+++ src/cfehome/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+import pathlib
+from django.shortcuts import render
+from visits.models import PageVisit
+
+
+this_dir=pathlib.Path(__file__).resolve().parent
+def home_page_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
+    qs=PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) /qs.count()
+    except:
+        percent = 0
+
+    my_title = "My Page"
+    html_temlate = "home.html"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": percent,
+        "total_visit_count": qs.count(),
+    }
+
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_temlate, my_context)

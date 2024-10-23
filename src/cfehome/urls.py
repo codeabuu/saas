96650265e4cc393083e3f57a8b1fe78ddb94c4dc
codeabuu <abdulngui718@gmail.com>
2024-10-23 18:20:25 -0400

    better authentication

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -4,9 +4,9 @@ from .views import home_view, about_view
 from auth import views as auth_views
 
 urlpatterns = [
-    path("", home_view), #index page -> root page
-    path("login/", auth_views.login_view),
-    path("register/", auth_views.register_view),
+    path("", home_view, name='home'), #index page -> root page
+    path("login/", auth_views.login_view, name='account_login'),
+    path("register/", auth_views.register_view, name='account_signup'),
     path("about/", about_view),
     path("hello-world/", home_view),
     path("hello-world.html", home_view),

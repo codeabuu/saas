2be335453b87dfdb8cea4ee0507dcf6b66f10779
codeabuu <abdulngui718@gmail.com>
2024-11-13 15:59:33 -0500

    A Django Session Based Checkout Process

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -3,11 +3,18 @@ from django.urls import path, include
 from .views import home_view, about_view, pw_protected_view, user_only_view, staff_only_view
 from auth import views as auth_views
 from subscriptions import views as subscription_views
+from checkouts import views as checkout_views
 
 urlpatterns = [
     path("", home_view, name='home'), #index page -> root page
     # path("login/", auth_views.login_view, name='account_login'),
     # path("register/", auth_views.register_view, name='account_signup'),
+    path("checkout/sub-price/<int:price_id>/", checkout_views.product_price_redirect_view,
+         name="sub-price-checkout"),
+    path("checkout/start", checkout_views.checkout_redirect_view,
+         name='stripe-checkout-start'),
+    path("checkout/success", checkout_views.checkout_finalize_view,
+         name="stripe-checkout-end"),
     path("pricing", subscription_views.subscription_price_view, name='pricing'),
     path("pricing/<str:interval>", subscription_views.subscription_price_view, name='pricing_interval'),
     path("about/", about_view),
--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import Group, Permission
 from django.db.models.signals import post_save
 from django.conf import settings
 import helpers.billing
+from django.urls import reverse
 
 User = settings.AUTH_USER_MODEL
 
@@ -102,6 +103,9 @@ class SubscriptionPrice(models.Model):
     class Meta:
         ordering = ['order', 'featured', '-updated']
 
+    def get_checkout_url(self):
+        return reverse("sub-price-checkout",
+                    kwargs={"price_id": self.id})
     @property
     def display_features_list(self):
         if not self.subscription:

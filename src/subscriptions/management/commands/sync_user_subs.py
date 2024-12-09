a38c48f173151a8c828d1935e38a2c3c97600283
Abdullahi Ngui <125456974+codeabuu@users.noreply.github.com>
2024-12-09 16:19:00 -0500

    correct function import from utils

--- src/subscriptions/management/commands/sync_user_subs.py
+++ src/subscriptions/management/commands/sync_user_subs.py
@@ -26,7 +26,7 @@ class Command(BaseCommand):
             subs_utils.clear_dangling_subs()
         else:
             print("Sync active subs")
-            done = subs_utils.refresh_active_users_subscriptions(
+            done = subs_utils.refresh_users_subscriptions(
                 active_only=True, 
                 days_left=days_left,
                 days_ago=days_ago,

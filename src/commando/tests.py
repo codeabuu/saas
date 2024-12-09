f76485094561fbaab259259729e73fb506a1665b
codeabuu <abdulngui718@gmail.com>
2024-12-09 18:08:55 -0500

    solved db url not loading on tests

--- src/commando/tests.py
+++ src/commando/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from django.conf import settings
+import os
+from decouple import config
 
 # Create your tests here.
+
+os.environ["DATABASE_URL"] = config("DATABASE_URL")
 class NeonDBTestCase(TestCase):
     def test_db_url(self):
         DATABASE_URL=settings.DATABASE_URL
